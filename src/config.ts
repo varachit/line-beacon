9821dcc34795e94aa7c4fe78de2faa51aa0c961f
Varachit W <52562559+varachit@users.noreply.github.com>
2022-09-24 23:15:27 +0700

    refactor: make port required in .env

--- src/config.ts
+++ src/config.ts
@@ -3,11 +3,12 @@ import invariant from 'tiny-invariant';
 
 dotenv.config();
 
+invariant(process.env.PORT, 'port is missing from environment variable');
 invariant(process.env.channelAccessToken, 'channelAccessToken is missing from environment variable');
 invariant(process.env.channelSecret, 'channelSecret is missing from environment variable');
 
 const config = {
-  port: process.env.port || 3000,
+  port: process.env.PORT,
   line: {
     channelAccessToken: process.env.channelAccessToken,
     channelSecret: process.env.channelSecret
